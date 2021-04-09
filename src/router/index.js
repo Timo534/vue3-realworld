7138ff2bfa9de3b2378063134a492aa6808b001c
qiuruiqiang <qiuruiqiang@do1.com.cn>
2021-04-09 17:53:12 +0800

    初始化项目模板

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,39 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Home,
+    children: [
+      {
+        path: '',
+        name: 'ArticleList',
+        component: () => import('../views/ArticleList.vue')
+      },
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: 'profile',
+        name: 'Profile',
+        component: () => import('../views/Profile.vue')
+      },
+      {
+        path: 'settings',
+        name: 'Settings',
+        component: () => import('../views/Settings.vue')
+      },
+      {
+        path: 'editArticle',
+        name: 'EditArticle',
+        component: () => import('../views/EditArticle.vue')
+      },
+      {
+        path: 'article',
+        name: 'Article',
+        component: () => import('../views/Article.vue')
+      }
+    ]
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <NavHeader></NavHeader>
+
+  <router-view></router-view>
+
+  <FooterBottom></FooterBottom>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavHeader from '@/components/NavHeader.vue'
+import FooterBottom from '@/components/FooterBottom.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NavHeader,
+    FooterBottom
   }
 }
 </script>
